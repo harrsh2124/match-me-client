f6c5e8bea5fa4962ad18714e7f7f2999f7cf5cc1
Harrsh Patel <me.harrsh.patel@gmail.com>
2022-06-01 23:13:58 +0530

    Integrated redux

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { CssBaseline, ThemeProvider, Typography } from '@mui/material';
 import React from 'react';
-import { darkTheme } from './utils/theme';
+import { useSelector } from 'react-redux';
+import { darkTheme, lightTheme } from './utils/theme';
 
 const App = () => {
+    const { mode } = useSelector((state) => state.theme);
+
     return (
-        <ThemeProvider theme={darkTheme}>
+        <ThemeProvider theme={mode === 'light' ? lightTheme : darkTheme}>
             <CssBaseline />
 
             <Typography component="p">Hello sir...</Typography>
