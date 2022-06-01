fd7e799a2ab17eb5de0b7338c49db847adc4ee8b
Harrsh Patel <me.harrsh.patel@gmail.com>
2022-06-02 00:35:42 +0530

    Added middlewares

--- src/store.js
+++ src/store.js
@@ -1,8 +1,21 @@
+import { createLogger } from 'redux-logger';
+import thunk from 'redux-thunk';
 import { configureStore } from '@reduxjs/toolkit';
 import themeReducer from './slices/theme/themeSlice';
+
+const logger = createLogger({
+    duration: true,
+    diff: true
+});
+
+const middleware = [];
+middleware.push(logger);
+middleware.push(thunk);
+const enhancers = [...middleware];
 
 export const store = configureStore({
     reducer: {
         theme: themeReducer
-    }
+    },
+    middleware: enhancers
 });
