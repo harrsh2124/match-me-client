4c39254b862debf5d1676116a84ce15bcdb80f86
Harrsh Patel <me.harrsh.patel@gmail.com>
2022-06-01 22:57:57 +0530

    Added MUI

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { CssBaseline, ThemeProvider, Typography } from '@mui/material';
 import React from 'react';
+import { darkTheme } from './utils/theme';
 
 const App = () => {
-    return <div>App</div>;
+    return (
+        <ThemeProvider theme={darkTheme}>
+            <CssBaseline />
+
+            <Typography component="p">Hello sir...</Typography>
+        </ThemeProvider>
+    );
 };
 
 export default App;
