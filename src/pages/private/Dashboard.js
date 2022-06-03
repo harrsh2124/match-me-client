2ccecdc48e7d0f5d119c9cf7e047f2760555e9af
harrsh2124 <me.harrsh.patel@gmail.com>
2022-06-03 16:58:21 +0530

    Added list in dashboard

--- src/pages/private/Dashboard.js
+++ src/pages/private/Dashboard.js
@@ -1,4 +1,5 @@
-import React, { useEffect } from 'react';
+import { Box, Typography } from '@mui/material';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { handleFetchUsersList } from '../../slices/dashboard/usersListSlice';
 
@@ -6,18 +7,40 @@ const Dashboard = () => {
     const { users } = useSelector((state) => state.usersList);
     const dispatch = useDispatch();
 
-    useEffect(() => {
-        if (!users.length) {
-            console.log(users);
-            console.log('No users');
-            dispatch(handleFetchUsersList());
-        }
-    }, []);
+    const [usersList, setUsersList] = useState([]);
 
     useEffect(() => {
-        console.warn('WARNINGGGGGGGGGGGGGGGGGGG');
-    }, []);
-    return <div>Dashboard</div>;
+        if (!users.length) {
+            console.log('No users');
+            dispatch(handleFetchUsersList());
+        } else {
+            console.log(users);
+            setUsersList((prevUsers) => {
+                return [...prevUsers, ...users];
+            });
+        }
+
+        // eslint-disable-next-line
+    }, [users]);
+
+    return (
+        <Box>
+            {usersList.map((user) => {
+                return (
+                    <Box
+                        key={user._id}
+                        sx={{
+                            display: 'flex',
+                            gap: '0.5rem'
+                        }}
+                    >
+                        <Typography>{user.firstName}</Typography>
+                        <Typography>{user.lastName}</Typography>
+                    </Box>
+                );
+            })}
+        </Box>
+    );
 };
 
 export default Dashboard;
